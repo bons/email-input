4a7273de8f6fdf2b4b33d2fcb2ec194cd21c2dbd
Juan Ignacio <beogip@gmail.com>
2015-03-05 19:55:47 -0300

    added tests

--- test/inputEmailSpec.js
+++ test/inputEmailSpec.js
@@ -13,8 +13,8 @@ describe('Unit Test: bInputEmail', function() {
     })
   );
 
-  it("should autocomplete gmail", function() {
-
+  it("should autocomplete gmail", function()
+  {
     var body  = document.querySelector("body");
     body.innerHTML = '<div b-input-email ng-init="inputEmail.email=\'foo@g\'"><input type="text" placeholder="example@email.com" ng-model="inputEmail.email"></div>';
 
@@ -24,4 +24,69 @@ describe('Unit Test: bInputEmail', function() {
     expect($rootScope.inputEmail.autocomplete).toBe("mail.com");
   });
 
+  it("should autocomplete hotmail", function()
+  {
+    var body  = document.querySelector("body");
+    body.innerHTML = '<div b-input-email ng-init="inputEmail.email=\'foo@h\'"><input type="text" placeholder="example@email.com" ng-model="inputEmail.email"></div>';
+
+    $compile(body)($rootScope);
+
+    expect($rootScope.inputEmail.email).toBe("foo@h");
+    expect($rootScope.inputEmail.autocomplete).toBe("otmail.com");
+  });
+
+  it("should autocomplete outlook", function()
+  {
+    var body  = document.querySelector("body");
+    body.innerHTML = '<div b-input-email ng-init="inputEmail.email=\'foo@o\'"><input type="text" placeholder="example@email.com" ng-model="inputEmail.email"></div>';
+
+    $compile(body)($rootScope);
+
+    expect($rootScope.inputEmail.email).toBe("foo@o");
+    expect($rootScope.inputEmail.autocomplete).toBe("utlook.com");
+  });
+
+  it("should autocomplete yahoo", function()
+  {
+    var body  = document.querySelector("body");
+    body.innerHTML = '<div b-input-email ng-init="inputEmail.email=\'foo@y\'"><input type="text" placeholder="example@email.com" ng-model="inputEmail.email"></div>';
+
+    $compile(body)($rootScope);
+
+    expect($rootScope.inputEmail.email).toBe("foo@y");
+    expect($rootScope.inputEmail.autocomplete).toBe("ahoo.com");
+  });
+
+  it("should autocomplete aol", function()
+  {
+    var body  = document.querySelector("body");
+    body.innerHTML = '<div b-input-email ng-init="inputEmail.email=\'foo@a\'"><input type="text" placeholder="example@email.com" ng-model="inputEmail.email"></div>';
+
+    $compile(body)($rootScope);
+
+    expect($rootScope.inputEmail.email).toBe("foo@a");
+    expect($rootScope.inputEmail.autocomplete).toBe("ol.com");
+  });
+
+  it("should autocomplete live", function()
+  {
+    var body  = document.querySelector("body");
+    body.innerHTML = '<div b-input-email ng-init="inputEmail.email=\'foo@l\'"><input type="text" placeholder="example@email.com" ng-model="inputEmail.email"></div>';
+
+    $compile(body)($rootScope);
+
+    expect($rootScope.inputEmail.email).toBe("foo@l");
+    expect($rootScope.inputEmail.autocomplete).toBe("ive.com");
+  });
+
+  it("should autocomplete icloud", function()
+  {
+    var body  = document.querySelector("body");
+    body.innerHTML = '<div b-input-email ng-init="inputEmail.email=\'foo@i\'"><input type="text" placeholder="example@email.com" ng-model="inputEmail.email"></div>';
+
+    $compile(body)($rootScope);
+
+    expect($rootScope.inputEmail.email).toBe("foo@i");
+    expect($rootScope.inputEmail.autocomplete).toBe("cloud.com");
+  });
 });
