ac07a9726b283185655823f624814428008d5169
Juan Ignacio <beogip@gmail.com>
2015-03-05 19:54:04 -0300

    Added karma in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,14 +19,25 @@ module.exports = function(grunt){
                 src: ['<%= srcFiles %>'],
                 dest: 'build/<%= pkg.name %>.min.js'
             }
+        },
+        karma: {
+          continuous: {
+            configFile: 'karma.conf.js',
+            singleRun: true
+          },
+          unit: {
+            configFile: "karma.conf.js"
+          }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
+    grunt.loadNpmTasks('grunt-karma');
+
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'concat:prod']);
+    grunt.registerTask('default', ['uglify', 'concat:prod', 'karma:continuous']);
 
     grunt.loadNpmTasks('grunt-contrib-concat');
 
